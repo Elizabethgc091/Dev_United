7b3d49965fc802011dd1ef689400dd96f4f2d680
Elizabeth Gonzalez Cruz <elizabeth.gc091@gmail.com>
2022-03-26 20:12:26 -0600

    add configuration firebase configuration

---
+++ src/firebaseService/firebase.js
@@ -0,0 +1,14 @@
+import firebase from "firebase";
+import { firebaseConfig } from "./config";
+import "firebase/firestore";
+import "firebase/auth";
+
+firebase.initializeApp(firebaseConfig);
+//data base firestore
+export const db = firebase.firestore();
+//authentication
+export const auth = firebase.auth();
+export const provider = new firebase.auth.GoogleAuthProvider();
+export const loginWithGoogle = () => auth.signInWithPopup(provider);
+export const logout = () => auth.signOut();
+export default firebase;
