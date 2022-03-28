f144b9203ecbf05de6a6d822be4de74a14b72919
Elizabeth Gonzalez Cruz <elizabeth.gc091@gmail.com>
2022-03-27 21:00:56 -0600

    add UI home page for desktop and mobile

--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -1,15 +1,24 @@
 /** Dependencies */
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+
 /** Style */
 import "./home.css";
+
 /** Firebase Services */
 import {
   auth,
   loginWithGoogle,
   logout,
 } from "../../../firebaseService/firebase";
+
+/** Router */
 import { Navigate } from "react-router-dom";
+
+/** Sources */
+import logoDevUnited from "../../../sources/icons/logoDevUnited.svg";
+import textLogo from "../../../sources/icons/textLogo.svg";
+import iconoGoogle from "../../../sources/icons/iconoGoogle.svg";
 
 export default function Home() {
   const navigate = useNavigate();
@@ -19,28 +28,40 @@ export default function Home() {
     setUser(user);
   });
 
-  const handleClick = () => {};
-
   return (
     <>
       {user === null ? (
         <div className="home-page">
           <div className="box-1">
             <div className="container-logo">
-              <div>logo</div>
-              <div>texto</div>
+              <img id="logo-dev-united" src={logoDevUnited} alt="logoDev" />
+              <img id="text-logo" src={textLogo} alt="DEV_UNITED" />
             </div>
           </div>
           <div className="box-2">
             <div className="container-login">
-              <div>Lorem, ipsum dolor.</div>
-              <div>Lorem ipsum dolor, sit amet consectetur adipisicing.</div>
-              <div>
-                <button type="button" onClick={user ? logout : loginWithGoogle}>
-                  sign in{" "}
+              <div className="container-welcome">
+                <span>Welcome</span> <br />
+                <span>to red social ✌🏽</span>
+              </div>
+              <div className="description-text">
+                Is a social network dedicated to programmers, to communicate
+              </div>
+              <div className="btn-signIn">
+                <img id="icon-google" src={iconoGoogle} alt="" />
+                <button
+                  type="button"
+                  id="btn-login"
+                  onClick={user ? logout : loginWithGoogle}
+                >
+                  Sign in with Google
                 </button>
               </div>
-              <footer>Lorem, ipsum dolor.</footer>
+              <footer className="footer">
+                <p>
+                  © 2022 Devs_United <span id="beta">-BETA</span>{" "}
+                </p>
+              </footer>
             </div>
           </div>
         </div>
