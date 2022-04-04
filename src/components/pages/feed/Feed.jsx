0045f7b3493f6635213ac0c84466dc414fe60554
Elizabeth Gonzalez Cruz <elizabeth.gc091@gmail.com>
2022-04-03 23:28:32 -0500

    fix user photo

--- src/components/pages/feed/Feed.jsx
+++ src/components/pages/feed/Feed.jsx
@@ -5,13 +5,7 @@ import { useNavigate } from "react-router-dom";
 /** components */
 import TweetCard from "./TweetCard";
 
-
-/** Firebase */
-import { auth, db, } from "../../../firebaseService/firebase";
-
-
 /** funcionalities */
-//import { addTweet, deleteTweet } from "../../../functionalities/funcionalities";
 import { getTweets, addTweet, deleteTweet } from "../../../functionalities/funcionalities"
 
 /** Style */
@@ -27,55 +21,23 @@ export default function Feed() {
   const navigate = useNavigate();
   const [message, setMessage] = useState("");
   const [timeLine, setTimeLine] = useState([]);
-  const [authUser, setAuthUser] = useState({});
-  const [userData, setUserData] = useState({})
+
+
   const CHAR_LIMIT = 200;
 
   useEffect(() => {
     if (user === null) { navigate("/"); }
-  }, [user])
-
-
-  useEffect(() => {
     getTweets(setTimeLine)
-  }, [])
+  }, [user])
 
   function handleSubmit(event) {
     event.preventDefault();
     addTweet(message);
     setMessage("");
   }
-  /*   useEffect(() => {
-      const desuscribir = db.collection("tweets")
-        .orderBy("created_at", "desc")
-        .onSnapshot((querySnapshot) => {
-          const tweets = [];
-          querySnapshot.forEach((doc) => {
-            const id = doc.id;
-            const temp = {
-              id,
-              ...doc.data(),
-            };
-            tweets.push(temp);
-          });
-          setTimeLine(tweets);
-        });
-  
-      auth.onAuthStateChanged((user) => {
-        setAuthUser(user);
-  
-        db.collection("users").doc(user.uid).get().then((response) => {
-          setUserData(response.data())
-        })
-      });
-      return () => {
-        desuscribir();
-      }
-    }, []);
-    console.log("usuario autentivado"); */
 
   /**
-   * @description función que guarda un tweet
+   * @description función que valida el tamañano de un tweet
    */
   function handleChange(event) {
     const { value } = event.target;
@@ -95,34 +57,18 @@ export default function Feed() {
 
   }
 
-  /*  function likeTweet(id, likesCount) {
-     console.log(userData.favorites);
-     // si ya le dio like, se hace una operacion de dislike (tweet -  1,  usuario elimina de lista de favs)
-     if (userData.favorites.includes(id)) {
- 
-       console.log("diste dislike")
-       db.collection("tweets").doc(id).update({ likesCount: likesCount - 1 })
-       db.collection("users").doc(auth.currentUser.uid).update({ favorites: [] })
-     } else {
-       console.log("Diste un like");
-       db.collection("tweets").doc(id).update({ likesCount: likesCount + 1 })
-       db.collection("users").doc(auth.currentUser.uid).update({ favorites: [id] })
-     }
- 
-   } */
-
   return (
     <>
       <div className="header-container">
         <div className="nav-container">
-          <img id="photoURL" src={authUser.photoURL} alt="foto-perfil" onClick={userPerfil} />
+          <img id="photoURL" src={user.photoURL} alt="foto-perfil" onClick={userPerfil} />
           <img id="logo-feed" src={logoDevUnited}></img>
           <img id="text-logo-feed" src={textLogo} alt="" />
         </div>
       </div>
       <div className="form-container">
         <div className="tweet-container">
-          <img id="photoURLtweet" src={authUser.photoURL} alt="foto-perfil" />
+          <img id="photoURLtweet" src={user.photoURL} alt="foto-perfil" />
           <div>
             <textarea
               id="text-area-tweet"
